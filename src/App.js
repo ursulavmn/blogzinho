3913a7700a23d12063436a2436ce7f5225d1035a
ursula <ursulavmn@gmail.com>
2023-03-17 08:34:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 
 //meus imports
 import {BrowserRouter, Routes, Route, Navigate, Router} from 'react-router-dom';
-
+import Home from "./firebase/pages/Home/Home";
+import About from "./firebase/pages/About/About";
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
      <div className='container'>
-      <Router>
-        
-      </Router>
+      <Routes>
+      <Route path ="/" element={<Home />}  />
+      <Route path ="/About" element={<About />}/>
+      </Routes>
      </div>
      </BrowserRouter>
     </div>

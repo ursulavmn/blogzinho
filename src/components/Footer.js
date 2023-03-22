3bbe2aa66c4daa3c77586dc6fae9d06194e8297c
ursula <ursulavmn@gmail.com>
2023-03-22 13:57:48 -0300

    novas atualizações

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import styles from './Footer.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Footer = () => {
   return (
-   <footer className={ styles.footer} >
+   <footer className={styles.footer} >
     <h3>Escreva sobre o que voce tem  interesse</h3>
     <p>Blogzinho</p>
    </footer>
